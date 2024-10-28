2186e8206a557cf42db44d2f723bbcdf11d559b0
LxHouse <364403299@qq.com>
2024-10-28 09:53:43 +0800

    feat:init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-center font-bold underline">
+        Hello Foundry !
+      </h1>
+    </div>
+  );
+}
+
+export default App;
