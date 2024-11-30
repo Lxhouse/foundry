3f0e58c415f9efa4438e472e32a6961d439f27ac
LxHouse <364403299@qq.com>
2024-11-30 11:47:34 +0800

    feat: 优化代码

--- src/pages/mainBody/Edit/Layer.tsx
+++ src/pages/mainBody/Edit/Layer.tsx
@@ -4,6 +4,7 @@ import { EyeInvisibleOutlined, LockOutlined } from '@ant-design/icons';
 import useEditStore from '@/store/useEditStore';
 import SortableContainer from '@/components/DragSortable/sortableContainer';
 import SortableItem from '@/components/DragSortable/SortableItem';
+
 const Layer: React.FC = () => {
   const {
     selectedId,
@@ -12,7 +13,8 @@ const Layer: React.FC = () => {
     changeSelectedId,
     changeComponentOrder,
   } = useEditStore();
-  const [changingTitleId, setChangingTitleId] = useState('');
+
+  const [changingTitleId, setChangingTitleId] = useState<string>('');
 
   // 处理组件标题点击逻辑
   const handleTitleClick = (fe_id: string, isHidden: boolean) => {
@@ -23,11 +25,14 @@ const Layer: React.FC = () => {
     selectComponent(fe_id);
     setChangingTitleId(fe_id === selectedId ? fe_id : '');
   };
+
+  // 选中组件
   const selectComponent = (fe_id: string) => {
     if (fe_id !== selectedId) {
       changeSelectedId(fe_id);
     }
   };
+
   // 更新组件状态
   const handleStateChange = (
     fe_id: string,
@@ -41,7 +46,7 @@ const Layer: React.FC = () => {
   };
 
   // 渲染标题输入框或静态标题
-  const renderTitle = (fe_id: string, title: string | undefined) => {
+  const renderTitle = (fe_id: string, title?: string) => {
     if (changingTitleId === fe_id) {
       return (
         <Input
@@ -69,9 +74,7 @@ const Layer: React.FC = () => {
         <SortableItem key={fe_id} id={fe_id}>
           <div
             className="flex justify-between items-center p-2 border-b border-gray-200"
-            onClick={() => {
-              selectComponent(fe_id);
-            }}
+            onClick={() => selectComponent(fe_id)} // 点击组件选择
           >
             {/* 左侧标题 */}
             <span
@@ -79,27 +82,29 @@ const Layer: React.FC = () => {
                 selectedId === fe_id ? 'text-blue-400 font-bold' : ''
               }`}
               onClick={(event) => {
-                event.stopPropagation();
-                handleTitleClick(fe_id, isHidden);
+                event.stopPropagation(); // 防止冒泡
+                handleTitleClick(fe_id, isHidden); // 点击标题时的处理
               }}
             >
-              {renderTitle(fe_id, title)}
+              {renderTitle(fe_id, title)} {/* 渲染标题 */}
             </span>
 
             <span className="flex items-center gap-4">
+              {/* 隐藏组件按钮 */}
               <Button
                 shape="circle"
                 disabled={selectedId !== fe_id}
                 type={isHidden ? 'primary' : 'default'}
                 icon={<EyeInvisibleOutlined />}
-                onClick={() => handleStateChange(fe_id, 'isHidden', !isHidden)}
+                onClick={() => handleStateChange(fe_id, 'isHidden', !isHidden)} // 切换隐藏状态
               />
+              {/* 锁定组件按钮 */}
               <Button
                 shape="circle"
                 disabled={selectedId !== fe_id}
                 type={isLocked ? 'primary' : 'default'}
                 icon={<LockOutlined />}
-                onClick={() => handleStateChange(fe_id, 'isLocked', !isLocked)}
+                onClick={() => handleStateChange(fe_id, 'isLocked', !isLocked)} // 切换锁定状态
               />
             </span>
           </div>
